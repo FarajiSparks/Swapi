54d276f4bb1ea0a731553b5957f5e8873e5608f9
Faraji <farajisparks@gmail.com>
2023-06-07 15:16:55 -0500

    Deck data logic and redux

--- src/actions/deckAction.js
+++ src/actions/deckAction.js
@@ -1,14 +1,26 @@
+//Deck Actions
 
-
-export const loadDecks =()=>({
-    type: 'LOAD_DECKS', 
-})
-
-
-export const createDeck = (faction, cards)  =>({
+export const addDeck = (name, faction) => ({
     type: 'ADD_DECK',
-    payload:{
-        faction, 
-        cards
-    }
-});
+    payload: { name, faction }
+   });
+   
+export const editDeck = (deckId, updatedDeck) => ({
+    type: 'EDIT_DECK',
+    payload: { deckId, updatedDeck }
+   });
+   
+export const deleteDeck = (deckId) => ({
+    type: 'DELETE_DECK',
+    payload: deckId
+   });
+
+export const loadDeck = (decks) => ({
+    type: 'LOAD_DECK',
+    payload: decks
+    });
+
+export const searchDeck = (name) =>({
+    type: 'SEARCH_DECK',
+    payload: name
+});    
--- src/reducers/deckReducer.js
+++ src/reducers/deckReducer.js
@@ -1,14 +1,46 @@
-const initialState = {
-    decks:[]
-}
 
-const deckReducer = (state=initialState, action)=>{
-    switch(action.type){
-        case "ADD_DECK":
+
+const initialState = {
+ decks: []
+};
+
+const deckReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_DECK':
+            const newDeck = {
+                id: state.decks.length + 1,
+                name: action.payload.name,
+                faction: action.payload.faction
+            };
+            return {
+                ...state,
+                decks: [...state.decks, newDeck]
+            };
+        case 'EDIT_DECK':
+            return {
+                ...state,
+                decks: state.decks.map(deck =>
+                deck.id === action.payload.deckId ? action.payload.updatedDeck : deck
+                )
+        };
+        case 'DELETE_DECK':
+            return {
+                ...state,
+                decks: state.decks.filter(deck => deck.id !== action.payload)
+                };
+        case 'LOAD_DECK':
             return{
-                ...state, decks: action.payload.detail
+                ...state, 
+                decks: state.decks
+            }
+        case 'SEARCH_DECK':
+            return{
+                ...state,
+                decks: state.decks.filter(deck=>deck.name === action.payload.name)
             }
         default:
-            return{...state}
+    return state;
     }
-}
+};
+
+export default deckReducer;
