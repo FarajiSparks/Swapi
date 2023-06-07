99d8f44952c6a53c9f532ce7bbb46c7cea36cf7a
Faraji <farajisparks@gmail.com>
2023-06-07 15:19:56 -0500

    Reducer export

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
 import peopleReducer from "./peopleReducer";
 import detailReducer from "./detailReducer";
+import deckReducer from "./deckReducer";
 
 const rootReducer = combineReducers({
     people: peopleReducer,
-    detail: detailReducer
+    detail: detailReducer,
+    decks: deckReducer
 });
 
 export default rootReducer;
