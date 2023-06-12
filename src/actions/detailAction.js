3b98d60f4d1f755ef3188d5868e2a98b2ee38718
Faraji <farajisparks@gmail.com>
2023-06-12 10:18:27 -0500

    Detail Vehicle and Starship Name Mapping Functionality

--- src/actions/detailAction.js
+++ src/actions/detailAction.js
@@ -5,19 +5,25 @@ import axios from 'axios';
  export const loadDetail = (url) => async (dispatch)=>{
     //Fetching Various Api Sections Through Urls from Detail Data
     const detailData = await axios.get(url);
-    const starShipData = await detailData.data.starships.map(async starshipUrl=> await axios.get(starshipUrl).then(data => {return data.data}));
+    const starShipUrls = await detailData.data.starships;
     const homeworldData = await axios.get(detailData.data.homeworld);
     const speciesData =  (detailData.data.species.length === 0) ? false : await axios.get(detailData.data.species);
-    const vehicleData = detailData.data.vehicles;
-   console.log(starShipData);
+    const vehicleUrls = detailData.data.vehicles;
+    const starShipData = await Promise.all(
+        starShipUrls.map((url) => axios.get(url))
+      );
+      const vehicleData = await Promise.all(
+        vehicleUrls.map((url) => axios.get(url))
+      );
+console.log(starShipData)
     dispatch({
         type: "GET_DETAIL",
         payload: {
             detail: detailData.data, 
-            starship: starShipData,
+            starship: starShipData.map((response) => response.data),
             homeworld: homeworldData.data,
             species: speciesData.data ? speciesData.data : '',
-            vehicles: vehicleData
+            vehicles: vehicleData.map((response) => response.data),
         }
     });
 };
